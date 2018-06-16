8ee8048b48a4b34c9f56cfb8f71301bb31c25730
elmerucr <evbaal@gmail.com>
2018-06-16 12:03:15 +0200

    implementing mmu

--- src/csg65ce02_mmu.c
+++ src/csg65ce02_mmu.c
@@ -3,5 +3,12 @@
 //  Created by elmerucr on 16/06/2018.
 //  Copyright © 2018 elmerucr. All rights reserved.
 
+#include <stdint.h>
 #include "csg65ce02_mmu.h"
 
+uint8_t *memory;
+
+
+// implementation of memory access function need to be implemented:
+// ...
+// ...
