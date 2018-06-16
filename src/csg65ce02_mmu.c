2a603692ffd7fcf3c385b74a87de349025c411e2
elmerucr <evbaal@gmail.com>
2018-06-16 09:58:12 +0200

    working on mmu

--- src/csg65ce02_mmu.c
+++ src/csg65ce02_mmu.c
@@ -2,3 +2,6 @@
 //
 //  Created by elmerucr on 16/06/2018.
 //  Copyright © 2018 elmerucr. All rights reserved.
+
+#include "csg65ce02_mmu.h"
+
--- src/csg65ce02_mmu.h
+++ src/csg65ce02_mmu.h
@@ -3,6 +3,12 @@
 //  Created by elmerucr on 16/06/2018.
 //  Copyright © 2018 elmerucr. All rights reserved.
 
+// Memory contains 512kb ram, physically addressable by mmu, divided in 128 x 4kb pages
+// Memory contains 128 banks of 4kb roms.
+// Implementation is as 1 array of exactly 1mb. The top half (msb put to 1) is rom
+//
+//
+
 #ifndef CSG65CE02_MMU_H
 #define CSG65CE02_MMU_H
 
