31c2343325cb39b6b01c74e1e7ed3b16c1865189
elmerucr <evbaal@gmail.com>
2018-06-17 14:39:19 +0200

    implementing mmu

--- src/csg65ce02_mmu.c
+++ src/csg65ce02_mmu.c
@@ -4,11 +4,25 @@
 //  Copyright © 2018 elmerucr. All rights reserved.
 
 #include <stdint.h>
+#include <stdlib.h>
 #include "csg65ce02_mmu.h"
 
-uint8_t *memory;
+uint8_t *csg65ce02_memoryblock;
 
+void csg65ce02_mmu_init() {
+	csg65ce02_memoryblock = malloc(1024 * 1024 * sizeof(uint8_t));			// get 1mb of storage space
+	// fill memory it with alternating pattern
+	for(int i=0; i<(1024*1024); i++) {
+		// fill blocks alternating with a number and zeroes
+		// lower half gets 0xff, upper half gets 0x80
+		csg65ce02_memoryblock[i] = (i & 64) ? ((i & ((1024 * 1024) / 2)) ? 0xff : 0x80) : 0x00;
+	}
+}
 
-// implementation of memory access function need to be implemented:
+void csg65ce02_mmu_cleanup() {
+	free(csg65ce02_memoryblock);
+}
+
+// memory access functions need to be implemented:
 // ...
 // ...
--- src/csg65ce02_mmu.h
+++ src/csg65ce02_mmu.h
@@ -4,12 +4,15 @@
 //  Copyright © 2018 elmerucr. All rights reserved.
 
 // Memory contains 512kb ram, physically addressable by mmu, divided in 128 x 4kb pages
-// Memory contains 128 banks of 4kb roms.
+// Memory may contain a max of 128 4kb roms.
 // Implementation is as 1 array of exactly 1mb. The top half (msb put to 1) is rom
 //
 //
 
 #ifndef CSG65CE02_MMU_H
 #define CSG65CE02_MMU_H
+
+void csg65ce02_mmu_init();
+void csg65ce02_mmu_cleanup();
 
 #endif
