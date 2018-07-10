e5727d053b46243ce15c127e91b9fe7f3de5e72f
elmerucr <evbaal@gmail.com>
2018-07-10 22:52:31 +0200

    implementing mmu

--- src/csg65ce02_mmu.c
+++ src/csg65ce02_mmu.c
@@ -12,31 +12,14 @@ uint8_t csg65ce02_mmu_registers[16];	// internal mem registers of mmu
 
 void csg65ce02_mmu_init() {
 	csg65ce02_ram = malloc(1024 * 1024 * sizeof(uint8_t));			// get 1mb of ram storage space
-	// fill memory it with alternating 0x00 and 0xff
+	// fill ram with alternating 0x00's and 0xff's
 	for(int i=0; i<(1024*1024); i++) {
 		csg65ce02_ram[i] = (i & 64) ? 0xff : 0x00;
 	}
 	for(int i=0; i<16; i++) {
 		csg65ce02_mmu_registers[i] = i;
 	}
-
-	// OBSOLETE --> needs removal, etc...
-	csg65ce02_ram[0x00000] = 0x00;
-	csg65ce02_ram[0x00001] = 0x01;
-	csg65ce02_ram[0x00002] = 0x02;
-	csg65ce02_ram[0x00003] = 0x03;
-	csg65ce02_ram[0x00004] = 0x04;
-	csg65ce02_ram[0x00005] = 0x05;
-	csg65ce02_ram[0x00006] = 0x06;
-	csg65ce02_ram[0x00007] = 0x07;
-	csg65ce02_ram[0x00008] = 0x08;
-	csg65ce02_ram[0x00009] = 0x09;
-	csg65ce02_ram[0x0000a] = 0x0a;
-	csg65ce02_ram[0x0000b] = 0x0b;
-	csg65ce02_ram[0x0000c] = 0x0c;
-	csg65ce02_ram[0x0000d] = 0x0d;
-	csg65ce02_ram[0x0000e] = 0x0e;
-	csg65ce02_ram[0x0000f] = 0x80;		// bit7=1 -> refers to ROM, bank 0
+	csg65ce02_mmu_registers[0x0f] = 0x80;
 }
 
 void csg65ce02_mmu_cleanup() {
@@ -48,12 +31,12 @@ uint8_t csg65ce02_read_byte(uint16_t address) {
 	uint8_t result;
 	switch((address & 0xff00) >> 8) {
 		default :
-			result =  csg65ce02_ram[(csg65ce02_ram[address >> 12] << 12) | (address & 0x0fff)];
+			result =  csg65ce02_ram[(csg65ce02_mmu_registers[address >> 12] << 12) | (address & 0x0fff)];
 	}
 	return result;
 }
 
 inline void csg65ce02_write_byte(uint16_t address, uint8_t byte) {
-	uint32_t phys_addr = (csg65ce02_ram[address >> 12] << 12) | (address & 0x0fff);
+	uint32_t phys_addr = (csg65ce02_mmu_registers[address >> 12] << 12) | (address & 0x0fff);
 	if(!(phys_addr & 0x00080000)) csg65ce02_ram[phys_addr] = byte;
 }
