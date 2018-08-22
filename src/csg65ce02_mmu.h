6179f9e6744874b97f9a0b224ab313b007c770b5
elmerucr <evbaal@gmail.com>
2018-08-22 09:35:56 +0200

    working on mmu

--- src/csg65ce02_mmu.h
+++ src/csg65ce02_mmu.h
@@ -3,10 +3,10 @@
 //  Created by elmerucr on 16/06/2018.
 //  Copyright © 2018 elmerucr. All rights reserved.
 
-// Memory contains 512kb ram, physically addressable by mmu, divided in 128 x 4kb blocks
-// Memory may contain a max of 128 4kb roms.
-// Implementation is as 1 array of exactly 1mb. The top half (msb put to 1) is rom
+// Memory contains 1024kb ram, physically addressable by mmu, divided in 256 x 4kb blocks
 //
+// Please note:
+// Memory access functions are declared in csg65ce02.h, implementation is in csg65ce02_mmu.c
 //
 
 #ifndef CSG65CE02_MMU_H
