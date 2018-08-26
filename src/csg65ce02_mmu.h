6e244752b72d5c839cfacb90883688868bf0be8c
elmerucr <evbaal@gmail.com>
2018-08-26 12:29:05 +0200

    small correction

--- src/csg65ce02_mmu.h
+++ src/csg65ce02_mmu.h
@@ -15,7 +15,7 @@
 #include "csg65ce02.h"
 
 uint8_t *csg65ce02_ram;		// will be publically visible (for dma purposes)
-							// should not be used directly by cpu
+							// should not be used directly by cpu (cpu uses access functions)
 
 void csg65ce02_mmu_init();
 void csg65ce02_mmu_cleanup();
